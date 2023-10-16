ef9ded4217792bba7f2fa22ba6f47dd3833b4b0a
You JaeWon <71857831+James0083@users.noreply.github.com>
2023-10-16 10:39:51 +0900

    slack web hook 시도

---
+++ ptc01_slack_web_hook.ts
@@ -0,0 +1,23 @@
+// https://hooks.slack.com/services/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
+
+import axios from 'axios';
+
+async function sendSlackNotification(message: string): Promise<void> {
+    let webhookUrl = 'https://hooks.slack.com/services/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';  // Replace with your actual Slack webhook URL
+    try {
+        await axios.post(webhookUrl, {
+        text: message,
+        });
+        console.log('Slack notification sent successfully.');
+    } catch (error) {
+        console.error('Error sending Slack notification:', error);
+    }
+}
+
+// const webhookUrl = 'https://hooks.slack.com/services/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';  // Replace with your actual Slack webhook URL
+const messageToSend = 'test message~~!';
+
+// sendSlackNotification(webhookUrl, messageToSend);
+// sendSlackNotification('test message in local~');
+
+module.exports = { sendSlackNotification };
