dda7cb80c9112e4ced06ff514a4bbeadc5fa5a60
You JaeWon <71857831+James0083@users.noreply.github.com>
2023-10-16 10:40:55 +0900

    logger, DB저장, slack web hook 적용

--- ptc01_main.ts
+++ ptc01_main.ts
@@ -1,26 +1,34 @@
 import schedule from 'node-schedule';
 import got from "got";
 import { cache } from './ptc01_cache';
-import { consoleLogger } from './ptc01_logger';
+import { consoleLogger, errorLogger } from './ptc01_logger';
 import { rejects } from 'assert';
+// import { candle_data, connectDB, disconnectDB, insertData, getAllData, saveDataToCandleCollection, saveDataToCandleLogCollection } from './ptc01_db';
+import { candle_data } from './ptc01_db';
 
 const sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
 
-const { connectDB, disconnectDB, insertData, getAllData } = require('./ptc01_db');
+const { connectDB, disconnectDB, insertData, getAllData, saveDataToCandleCollection, saveDataToCandleLogCollection } = require('./ptc01_db');
+const { sendSlackNotification } = require('./ptc01_slack_web_hook');
+
 const mongoUser: string = 'dnsever';
 const mongoUserPw: string = 'dnsever_pw';
 // const connectionString: string = 'mongodb://' + mongoUser + ':' + mongoUserPw + '@localhost:27017/candles';
 const connectionString: string = `mongodb://${mongoUser}:${mongoUserPw}@localhost:27017/candle`;
 
+//DB 연결
 connectDB(connectionString);
 
 let cnt: number = 0;
 let retry_cnt: number = 0;
 
+let candleDataSet: Set<candle_data> = new Set<candle_data>();
+
 // const interval:string = '1m';
 // const sort:string = 'desc';
 // const limit: number = 1;
 
+//public API에 요청, return response.body.data
 async function sendGetRequest(start_time: string, end_time: string, interval: string = '1m'
     , sort: string = 'desc', limit: number = 11) {
     
@@ -36,32 +44,60 @@ async function sendGetRequest(start_time: string, end_time: string, interval: st
         consoleLogger.log('statusCode : ' + response.statusCode);
 
         let rBody = JSON.parse(JSON.stringify(response.body));
-        monitoring_responseJSON(rBody);
         let rBodyData = rBody.data;
+        monitoring_responseJSON(rBodyData);
+        // console.log(rBodyData.length);
+
         
-        insertData(rBodyData[0]);
-        // insertData(rBodyData[1]);
+        saveInDB(rBodyData);
 
-        // console.log('Response body:', response.body);
-        // console.log('Response body data:', rBodyData[0].start_time, '+++' , rBodyData[1].start_time);
-
-        consoleLogger.log("cnt: " + (++cnt) + "==============");
-        // return rBodyData;
+        // candleDataSet.forEach(obj => console.log(obj));
+        consoleLogger.log('candleDataSet Size: ' + candleDataSet.size);
+        consoleLogger.log("==============cnt: " + (++cnt) + "==============");
+        // saveDataToCandleLogCollection("success!");
         return new Promise((resolve) => {
-            resolve(response);
+            // resolve(response.body);
+            resolve(rBodyData);
         })
         
     } catch (error) {
-        console.error('Error:', error);
+        let errorString = '[SendGetRequest Error] ' + error;
+        saveDataToCandleLogCollection(errorString);
+        errorLogger.log(errorString);
+        retry_cnt += 1;
         return new Promise((rejects) => {
             rejects(error);
         })
     }
 }
 
-function monitoring_responseJSON(responseBody: any) {
-    let rBodyData=responseBody.data;
-    consoleLogger.log('Response body data:'+ rBodyData[0].start_time + '+++' + rBodyData[1].start_time);
+//모니터링 로그
+function monitoring_responseJSON(rBodyData: any) {
+    // let rBodyData=responseBody.data;
+    // consoleLogger.log(rBodyData.length);
+    consoleLogger.log('Response body data times:'+ rBodyData[0].start_time + '~~~' + rBodyData[rBodyData.length-1].end_time);
+}
+
+//DB저장 
+function saveInDB(rBodyData: any) {
+    let skip_cnt = 0;
+    for (let cData of rBodyData) {
+        //동일한 데이터가 Set에 있는지 확인
+        if (!isObjectInSet(candleDataSet, cData)) {
+            try {
+                saveDataToCandleCollection(cData);
+                candleDataSet.add(cData);
+            } catch (error) {
+                errorLogger.log("Duplicate data. Skipping...");
+            }
+        }
+        else {
+            // consoleLogger.log("Duplicate data. Skipping...");
+            skip_cnt += 1;
+        }
+    }
+    consoleLogger.log("Duplicate data Skipped : " + skip_cnt);
+    skip_cnt = 0;
 }
 
 let date;
@@ -73,11 +109,17 @@ const requestBatch = schedule.scheduleJob('* * * * * *', () => {
     let end_time = date.toISOString();
     date.setMinutes(date.getMinutes() - 10);
     let start_time = date.toISOString();
-    console.log('running... / start: ' + start_time + '/ end: '+ end_time);
-    sendGetRequest(start_time, end_time).then((response) => {
-        // console.log("response:", response);
+
+    console.log('running...');
+    console.log('request time : ' + start_time + '~' + end_time);
+    sendGetRequest(start_time, end_time).then((responseData) => {
+        // console.log("response:", response.body);
+        // sendSlackNotification('test success message~');
+        // sendSlackNotification('\n' + JSON.stringify(responseData));
     }).catch((error) => {
-        console.log("error: ", error);
+        let errString = "[requestBatch Error] " + error
+        errorLogger.log(errString);
+        sendSlackNotification(errString);
     });
 
     // if(response.statusCode!=200) { retry_cnt+=1; }
@@ -92,11 +134,30 @@ const requestBatch = schedule.scheduleJob('* * * * * *', () => {
     // setTimeout(() => console.log("cnt: "+(++cnt)+"=============="), 2000);
 })
 
+//중복검사 (Set)
+function isObjectInSet(set: Set<candle_data>, targetObj: candle_data): boolean {
+  let found = false;
+
+  set.forEach(obj => {
+      if (obj.market_id === targetObj.market_id && obj.open === targetObj.open && obj.close === targetObj.close
+          && obj.low === targetObj.low && obj.high === targetObj.high
+          && obj.base_volume === targetObj.base_volume && obj.quote_volume === targetObj.quote_volume
+          && obj.start_time === targetObj.start_time && obj.end_time === targetObj.end_time) {
+
+          found = true;
+        return; // Break the loop early since we found a match
+    }
+  });
+
+  return found;
+}
+
+//Batch 종료 
 setTimeout(() => {
     requestBatch.cancel()
-    getAllData();
+    // getAllData();
     
     // setTimeout(disconnectDB(),5000);
-}, 4000); //4초 후 종료
+}, 1000*60*60*3); //ms 후 종료
 
     
